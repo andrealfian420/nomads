ea0b159d6eba9ed891f2ea95533273b2811b984e
andrealfian420 <andrealfian69699@gmail.com>
2020-02-14 22:40:36 +0700

    Modifikasi tampilan halaman sign in

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,29 @@
-@extends('layouts.success')
+@extends('layouts.login-layout')
 
 @section('title', 'Login Nomads')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card mt-4">
-                <div class="card-header text-center">{{ __('Login') }}</div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email"
-                                class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<div class="login-container">
+    <div class="container">
+        <div class="row page-login d-flex align-items-center">
+            <div class="section-left col-12 col-md-6">
+                <h1 class="mb-4">
+                    We explore the new <br> life much better
+                </h1>
+                <img src="{{url('frontend/images/login-image.png')}}" alt="Login Gallery" class="w-75 d-none d-sm-flex">
+            </div>
+            <div class="section-right col-12 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{url("frontend/images/logo.png")}}" alt="Logo Nomads" class="w-50 mb-4">
+                        </div>
+                        <form action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">Email Address</label>
+                                <input type="text" class="form-control @error('email') is-invalid
+                                    @enderror" name="email" value="{{ old('email') }}" id="email" required>
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -26,16 +31,11 @@
                                 </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password">
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                    name="password" id="password" required>
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -43,39 +43,27 @@
                                 </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" name="remember" id="remember"
+                                    {{ old('remember') ? 'checked' : '' }}>
+                                <label for="remember" class="form-check-label">Remember Me</label>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                            <button type="submit" class="btn btn-login btn-block">Sign In</button>
 
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                            @if (Route::has('password.request'))
+                            <p class="forgot-pwd text-center mt-4">
+                                <a href="{{ route('password.request') }}">Lupa password kamu?</a>
+                            </p>
+                            @endif
+
+                            <p class="text-center mt-4">
+                                Belum punya akun? <a href="{{route('register')}}">Daftar</a> Sekarang!
+                            </p>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <div class="text-center mt-2">
-                Don't have an account? <a href="{{route('register')}}">Register now!</a>
             </div>
         </div>
     </div>
