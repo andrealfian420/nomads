76320673c25ccc837fb1c79df982930dc3c08a94
andrealfian420 <andrealfian69699@gmail.com>
2020-02-15 16:25:09 +0700

    Modifikasi tampilan halaman forgot password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,44 +1,54 @@
-@extends('layouts.app')
+@extends('layouts.login-layout')
+
+@section('title', 'Register Nomads')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="login-container">
+    <div class="container">
+        <div class="row page-login d-flex align-items-center">
+            <div class="section-left col-12 col-md-6">
+                <h1 class="mb-4">
+                    Forgot your password? <br> Don't worry, we got it covered !
+                </h1>
+                <img src="{{url('frontend/images/forgot-image.png')}}" alt="Register Gallery"
+                    class="w-75 d-none d-sm-flex">
+            </div>
+            <div class="section-right col-12 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{url("frontend/images/logo.png")}}" alt="Logo Nomads" class="w-50 mb-4">
+                        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
+                        @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
+                        @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <form action="{{ route('password.email') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">Email Address</label>
+                                <input type="text" class="form-control @error('email') is-invalid
+                                        @enderror" name="email" value="{{ old('email') }}" id="email" required>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                            <button type="submit" class="btn btn-login btn-block">
+                                Send Confirmation Link
+                            </button>
+
+                            <p class="text-center mt-4">
+                                Sudah punya akun? Yuk <a href="{{route('login')}}">Masuk</a>
+                            </p>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
